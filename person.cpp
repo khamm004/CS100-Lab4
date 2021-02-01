3906fa1e8fc5d0fc75c15f7604f9ff66ebd41562
khamm004 <khamm004@ucr.edu>
2021-02-01 14:04:48 -0800

    Changes to fix errors: adjusted length of new name char array to be strlen(name_)+1, deleted name in destructor, deleted temp in both print_lineage and compute_relation, deleted *t in expand

--- person.cpp
+++ person.cpp
@@ -6,7 +6,7 @@ using std::cout;
 using std::endl;
 
 Person::Person(const char *name_, Person* father_, Person* mother_){
-    name = new char[strlen(name_)];
+    name = new char[strlen(name_)+1];
     strcpy(name, name_);
     father = father_;
     mother = mother_;
@@ -16,7 +16,8 @@ Person::Person(const char *name_, Person* father_, Person* mother_){
 }
 
 Person::~Person(){
-    delete children;
+    delete[] name;
+    delete[] children;
 }
 
 void Person::addChild(Person *newChild){
@@ -52,6 +53,7 @@ void Person::printLineage(char dir, int level){
             father->printLineage(dir, level + 1);
         }
     }
+    delete[] temp;
 }
 
 /* helper function to compute the lineage
@@ -59,15 +61,18 @@ void Person::printLineage(char dir, int level){
 * if level >= 1 then returns ("great ")^(level - 1) + "grand "
 */
 char* Person::compute_relation(int level){
+    
     if(level == 0) return strcpy(new char[1], "");
 
-    char *temp = strcpy(new char[strlen("grand ") + 1], "grand ");;
+    char *temp = strcpy(new char[strlen("grand ") + 1], "grand ");
     
     for(int i = 2; i <= level; i++){
         char *temp2 = new char[strlen("great ") + strlen(temp) + 1];
         strcat(strcpy(temp2, "great "), temp);
-        temp = temp2;
-    }
+        delete[] temp;
+	temp = temp2; 
+	  
+  }
     return temp;
 }
 
@@ -77,6 +82,7 @@ char* Person::compute_relation(int level){
 void expand(Person ***t, int *MAX){
   Person **temp = new Person*[2 * *MAX];
   memcpy(temp, *t, *MAX * sizeof(**t));
+  delete[] *t;
   *MAX *= 2;
   *t = temp;
 }
